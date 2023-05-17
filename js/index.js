69a4c684e3e9dfb90d61fbddfb0c5210c1abc623
T. Stark <99743288+Shadow-Captain@users.noreply.github.com>
2023-05-17 14:30:40 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-
+var typed = new Typed(".multiple-text", {
+    strings: ["Happy Hacking", "Web Developer", "Sonrie si te gusta el pito", "Blogger", "Doxxing"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
